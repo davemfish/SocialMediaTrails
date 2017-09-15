241121cf9b00a930fdfd518877d6fb83495e7946
davemfish <davemfish@gmail.com>
2017-09-14 20:28:02 -0700

    adding module to select a cell_size, as a function of bounding box and number of cells requested.

--- scripts/recmodel_client_parameters_boilerplate.py
+++ scripts/recmodel_client_parameters_boilerplate.py
@@ -7,10 +7,13 @@ InVEST version: 3.3.1b1.post10+nff1c8fdca089
 import natcap.invest.recreation.recmodel_client
 # natcap.invest source code available here:
 # https://bitbucket.org/natcap/invest/
+from choose_cellsize_parameter import choose_cellsize
+
+aoi = '../data/rainierNP_boundary_utm.shp'
 
 # 'aoi_path' and 'workspace_dir' are required arguments
 args = {
-        u'aoi_path': u'', # path to a shapefile
+        u'aoi_path': aoi, # path to a shapefile
         u'grid_aoi': False,
         u'grid_type': u'hexagon',
         u'cell_size': 7000.0,
@@ -22,6 +25,10 @@ args = {
         u'results_suffix': u'',
         u'workspace_dir': u'', # path to a directly 
 }
+
+cs = choose_cellsize(aoi, 100)
+args.cell_size = cs
+print(args)
 
 if __name__ == '__main__':
     natcap.invest.recreation.recmodel_client.execute(args)
